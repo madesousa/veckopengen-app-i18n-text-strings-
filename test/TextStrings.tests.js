37d283b21187a39326afee67955d3db30c61cdd4
anback <anders.back@me.com>
2016-03-02 10:57:24 +0100

    added some text strings

--- test/TextStrings.tests.js
+++ test/TextStrings.tests.js
@@ -11,6 +11,31 @@ describe("TextStrings", () => {
 	})
 
 	it("it should return no", () => {
-		expect(getTextStrings("no")).to.not.equal(undefined) 
+		expect(getTextStrings("no")).to.not.equal(undefined)
 	})
+
+	it("all textstrings in sv should have a row in en", () => {
+		var se = getTextStrings("sv")
+		var keys = Object.keys(se)
+		var en = getTextStrings("en")
+		keys.forEach(key => {
+			var str = en[key]
+			if (!str && str !== "")
+				console.log("Missing Key: " + key)
+			expect(en[key]).to.not.equal(undefined)
+		})
+	})
+
+	it("all textstrings in sv should have a row in no", () => {
+		var se = getTextStrings("sv")
+		var keys = Object.keys(se)
+		var no = getTextStrings("no")
+		keys.forEach(key => {
+			var str = no[key]
+			if (!str && str !== "")
+				console.log("Missing Key: " + key)
+			expect(no[key]).to.not.equal(undefined)
+		})
+	})
+
 })
