b00b29ac155455bcabe1d48caf95c38ff867754d
anback <anders.back@me.com>
2016-03-02 06:47:19 +0100

    i18n => text_strings

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var TextStrings_sv = require("./TextStrings_sv") 
-var TextStrings_no = require("./TextStrings_no") 
-var TextStrings_en = require("./TextStrings_en") 
+var TextStrings_sv = require("./text_strings/TextStrings_sv") 
+var TextStrings_no = require("./text_strings/TextStrings_no") 
+var TextStrings_en = require("./text_strings/TextStrings_en") 
 
 module.exports = lang => {
 	if (lang === "sv")
