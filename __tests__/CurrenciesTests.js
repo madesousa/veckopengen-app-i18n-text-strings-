dbcbe68bb6dcdf0e8dd2a6ac5e3ba06699bc7149
anback <anders.back@me.com>
2016-07-28 11:03:40 +0200

    getDefaultCurrency

--- __tests__/CurrenciesTests.js
+++ __tests__/CurrenciesTests.js
@@ -1,9 +1,14 @@
-import {getCurrencies} from "../index"
+import {getDefaultCurrency, getCurrencies} from "../index"
 jest.disableAutomock()
 
 describe("TextStrings", () => {
 	it("it should return Currencies",() => {
 		expect(getCurrencies()).not.toEqual(undefined)
 		expect(getCurrencies().length).toEqual(166)
+	})
+
+	it("it should return default currency", () => {
+		expect(getDefaultCurrency()).toEqual(43) //EUR
+		expect(getDefaultCurrency("SE")).toEqual(121) //SEK
 	})
 })
