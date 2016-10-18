27f0acea0fede0cd24cda6f5024bb4bab70fd74c
anback <anders.back@me.com>
2016-10-18 11:06:17 +0200

    Sara

---
+++ sara/src/sara.js
@@ -0,0 +1,36 @@
+var fs = require("fs")
+
+let templateDir = "./text_strings/client"
+let getPath = (file) => `${templateDir}/${file}`
+var TextStrings_sv_path = getPath("TextStrings_sv.json")
+var TextStrings_sv = fs.readFileSync(TextStrings_sv_path, {encoding : "utf8"})
+TextStrings_sv = JSON.parse(TextStrings_sv)
+
+let syncTextStrings = (file) => {
+
+
+  if(file === "TextStrings_default.json")
+    return
+
+  if(file === "TextStrings_sv.json")
+    return
+
+  var path = getPath(file)
+
+  var TextStrings = fs.readFileSync(path, {encoding : "utf8"})
+  TextStrings = JSON.parse(TextStrings)
+  TextStrings = {...TextStrings_sv, ...TextStrings}
+  TextStrings = JSON.stringify(TextStrings, undefined, 2)
+  fs.unlinkSync(path)
+  fs.writeFileSync(path, TextStrings, {encoding : "utf8"})
+  console.log(`Updated: ${path}`)
+}
+
+
+fs.readdirSync(templateDir).filter((file) => file !== "jkdfk")
+  .forEach((languageCode) => syncTextStrings(languageCode))
+
+//fix swedish TextStrings formatting
+TextStrings_sv = JSON.stringify(TextStrings_sv, undefined, 2)
+fs.unlinkSync(TextStrings_sv_path)
+fs.writeFileSync(TextStrings_sv_path, TextStrings_sv, {encoding : "utf8"})
