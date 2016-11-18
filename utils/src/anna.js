9674a8708f9b9e6e99629c59432353ce7d5d9fde
Martin <martinagnalv@gmail.com>
2016-11-18 13:27:51 +0100

    started working with anna

--- utils/src/anna.js
+++ utils/src/anna.js
@@ -2,15 +2,21 @@
 
 //@martin, alright, i would first do this as a seperate project called anna becuase we dont want to screw up sara and this might not work
 var fs = require("fs")
+var googleTranslate = require("google-translate")("apiKey")
+
 let templateDir = "./text_strings/client"
 let getPath = (file) => `${templateDir}/${file}`
+
+var TextStrings_default_path = getPath("TextStrings_default.json")
+var TextStrings_default = fs.readFileSync(TextStrings_default_path, {encoding : "utf8"})
+TextStrings_default = JSON.parse(TextStrings_default)
 
 var TextStrings_sv_path = getPath("TextStrings_sv.json")
 var TextStrings_sv = fs.readFileSync(TextStrings_sv_path, {encoding : "utf8"})
 TextStrings_sv = JSON.parse(TextStrings_sv)
 
 
-let translateTextStringForFile = (text_string, file) => {
+let translateTextStringForFile = (file, textId) => {
 
   //@martin see sara code below as inspiration, this file loops over all files in the directory ./text_strings/client/
   //on line 24, 25 the file gets read and parsed to a js object.
@@ -22,43 +28,38 @@ let translateTextStringForFile = (text_string, file) => {
 
   //one case you need to handle is the strings with "$1%s", perhaps the google lib wont translate it
 
+
   if(file === "TextStrings_default.json")
     return
 
   if(file === "TextStrings_sv.json")
     return
 
+  if(file !== "TextStrings_da.json")
+    return
+
+
   var path = getPath(file)
 
   var TextStrings = fs.readFileSync(path, {encoding : "utf8"})
   TextStrings = JSON.parse(TextStrings)
+  var stringToReplace = TextStrings[textId]
+  //var translatedString = googleTranslate.translate(stringToReplace)
+  //plocka ut textid och trnaslatea de till rätt lang
+  //replace gamla med nya
+  //sen spara till fil
+return
 
-  //Delete Support
-  Object.keys({...TextStrings})
-    .filter((key) => TextStrings_sv[key] === undefined)
-    .forEach((key) => {
-      delete TextStrings[key]
-      console.log(`Deleting key: '${key}' from ${file}`)
-    })
-
-  //Craete Support
-  var NewTextStrings = {...TextStrings_sv, ...TextStrings}
-  Object.keys(TextStrings_default).forEach(key => delete NewTextStrings[key])
-  var NewTextStringsLength = Object.keys(NewTextStrings).length
-  var TextStringsLength = Object.keys(TextStrings).length
-  var delta = NewTextStringsLength - TextStringsLength
-
-  if(delta > 0)
-    console.log(`Updated ${delta} textstrings in ${file}`)
 
   //Save changes
-  NewTextStrings = JSON.stringify(NewTextStrings, undefined, 2)
+  //TextStrings = JSON.stringify(TextStrings, undefined, 2)
   fs.unlinkSync(path)
-  fs.writeFileSync(path, NewTextStrings, {encoding : "utf8"})
+  fs.writeFileSync(path, TextStrings, {encoding : "utf8"})
 }
+
+let textIdToTranslate = process.argv[2]
+
 
 //@martin parse process.argv which contains the inputs you executed the app with, for example, npm run anna -- text_id,
 //then the value of textid will be in the process.argv array at place 3 or 4
-
-
-fs.readdirSync(templateDir).forEach((file) => translateTextStringForFile(file))
+fs.readdirSync(templateDir).forEach((file) => translateTextStringForFile(file, textIdToTranslate))
