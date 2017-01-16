da3f7f934f539ec90868ee4143b3aa3dafbada69
anback <anders.back@me.com>
2017-01-16 09:45:43 +0100

    fixed anna and added bar_code_scanner_help_text_parent/child

--- utils/src/anna.js
+++ utils/src/anna.js
@@ -8,24 +8,14 @@ let templateDir = './text_strings/client'
 let getPath = (file) => `${templateDir}/${file}`
 
 let translateTextStringForFile = (file, textId) => {
-  // @martin see sara code below as inspiration, this file loops over all files in the directory ./text_strings/client/
-  // on line 24, 25 the file gets read and parsed to a js object.
-  // In this function i would translate the text_string with the help of google transalte and save the file,
-  // the file is saved on line 49 and 50
-
-  // a note about this environment, it is a node 6 env. Node 6 differs from react native, for example import does not work, use require,
-  // node 6 api docs you can find online.
-
-  // one case you need to handle is the strings with "$1%s", perhaps the google lib wont translate it
-
   if (file === 'default.json') { return Promise.resolve() }
+  if (file.indexOf('.json') === -1) { return Promise.resolve() }
 
   if (file === 'sv.json') {
     return Promise.resolve()
   }
 
   var path = getPath(file)
-
   var TextStrings = fs.readFileSync(path, {encoding: 'utf8'})
   TextStrings = JSON.parse(TextStrings)
   var stringToReplace = TextStrings[textId]
