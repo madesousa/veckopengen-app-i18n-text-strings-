db8b3128360f598a0e46a77e6d88a8cbd32c300e
Anders Back <anders.back@me.com>
2017-02-22 07:51:26 +0100

    pulled getMomentLocale into the project

--- index.js
+++ index.js
@@ -6,11 +6,6 @@ import sv from './text_strings/client/sv.json'
 import no from './text_strings/client/nb.json'
 import en from './text_strings/client/en.json'
 import _default from './text_strings/client/default.json'
-import svLocale from 'moment/locale/sv'
-import daLocale from 'moment/locale/da'
-import fiLocale from 'moment/locale/fi'
-import nbLocale from 'moment/locale/nb'
-import enLocale from 'moment/locale/en-gb'
 import CountryCodes from './CountryCodes.json'
 import Regions from './Regions.json'
 import Cities from './Cities.json'
@@ -49,27 +44,6 @@ export let getTextStrings = (lang: string) => {
 
 export let getRegions = () => { return Regions }
 export let getCities = () => { return Cities }
-
-export let getMomentLocale = (locale: string) => {
-  if (locale === undefined) {
-    return enLocale
-  }
-
-  let lang = locale.substring(0, 2)
-  switch (lang) {
-    case 'sv':
-      return svLocale
-    case 'da':
-      return daLocale
-    case 'fi':
-      return fiLocale
-    case 'nb':
-      return nbLocale
-    case 'en':
-      return enLocale
-    default: return enLocale
-  }
-}
 
 export let getCountries = () => CountryCodes
 export let getCountry = (countryCode: string) => CountryCodes.find(country => country.code === countryCode)
