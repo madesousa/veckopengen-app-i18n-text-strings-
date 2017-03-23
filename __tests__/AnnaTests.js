0b4abdf0f588f2334fc30328361c9763c9bf470b
andersback <anders.back@me.com>
2017-03-23 06:54:54 +0100

    Added Anna requirements

--- __tests__/AnnaTests.js
+++ __tests__/AnnaTests.js
@@ -10,7 +10,10 @@ describe('AnnaHelper', () => {
   it('it should be able to call fromHash', () => {
     expect(fromHash('89438949034034 har utnyttjas 89438949034035 och har <boldGreen>89438949034032</boldGreen> kvar för att få belöningen på 89438949034033.')).toMatchSnapshot()
   })
+
   it('it should not have changed translation helper template ', () => {
-    expect(translationHelpTemplate).toEqual('*** Google Translated ***')
+    expect(translationHelpTemplate).toEqual('*** PLZ_TRANSLATE from en.json ***')
   })
+
+  xit('it should translate from en instead of sv', () => {})
 })
