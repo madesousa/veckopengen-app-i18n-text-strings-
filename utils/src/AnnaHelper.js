e9b3ecb36fd203b0b78db8fda2f8a081fdb20d41
hstamenov <hristo@gimitheapp.com>
2017-03-23 12:31:10 +0200

    change to PLZ_TRANSLATE

--- utils/src/AnnaHelper.js
+++ utils/src/AnnaHelper.js
@@ -1,4 +1,4 @@
-var symbolsThatShouldNotBeReplaced = ['%1$s', '%2$s', '%3$s', '%4$s', '%1$d', '%2$d', '%3$d', '%4$d', '*** Google Translated ***']
+var symbolsThatShouldNotBeReplaced = ['%1$s', '%2$s', '%3$s', '%4$s', '%1$d', '%2$d', '%3$d', '%4$d', 'PLZ_TRANSLATE']
 var hashPrefix = 8943894903403
 
 let toHash = (text: string) => {
@@ -19,5 +19,5 @@ let fromHash = (text: string) => {
   return text
 }
 
-let translationHelpTemplate = '*** Google Translated ***'
+let translationHelpTemplate = 'PLZ_TRANSLATE'
 module.exports = {toHash, fromHash, translationHelpTemplate}
