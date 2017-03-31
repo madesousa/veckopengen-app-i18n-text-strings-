8073ed20b2581a9cd0d0ee8c244c9b6dc89067ac
andersback <anders.back@me.com>
2017-03-31 06:49:07 +0200

    Removed snapshots from i18n

--- __tests__/AnnaTests.js
+++ __tests__/AnnaTests.js
@@ -2,11 +2,11 @@ import {toHash, fromHash, translationHelpTemplate, translateFrom} from '../utils
 
 describe('AnnaHelper', () => {
   it('it should be able to hash string', () => {
-    expect(toHash('%1$d har utnyttjas %2$d och har <boldGreen>%3$s</boldGreen> kvar för att få belöningen på %4$s.')).toMatchSnapshot()
+    expect(toHash('%1$d har utnyttjas %2$d och har <boldGreen>%3$s</boldGreen> kvar för att få belöningen på %4$s.')).toEqual('89438949034034 har utnyttjas 89438949034035 och har <boldGreen>89438949034032</boldGreen> kvar för att få belöningen på 89438949034033.')
   })
 
   it('it should be able to call fromHash', () => {
-    expect(fromHash('89438949034034 har utnyttjas 89438949034035 och har <boldGreen>89438949034032</boldGreen> kvar för att få belöningen på 89438949034033.')).toMatchSnapshot()
+    expect(fromHash('89438949034034 har utnyttjas 89438949034035 och har <boldGreen>89438949034032</boldGreen> kvar för att få belöningen på 89438949034033.')).toEqual('%1$d har utnyttjas %2$d och har <boldGreen>%3$s</boldGreen> kvar för att få belöningen på %4$s.')
   })
 
   it('it should not have changed translation helper template ', () => {
