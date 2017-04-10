0bd65a96b024f74be62364999b39fa0d0c0e370d
andersback <anders.back@me.com>
2017-04-10 13:40:58 +0200

    Added birgitta inconsistency test to server text strings

--- __tests__/TextStringsForServerTests.js
+++ __tests__/TextStringsForServerTests.js
@@ -1,5 +1,7 @@
-import {languageCodes} from '../index'
-import {compareKeys, compareKeysWithinTextStrings, checkTemplateLenght} from '../TestUtil'
+/* eslint no-console:0 */
+import {compareKeys, compareKeysWithinTextStrings, checkTemplateLenght, checkBirgittaInconsistencies} from '../TestUtil'
+import { getTextStrings, languageCodes } from '..'
+
 jest.disableAutomock()
 
 var textStringsTypes = ['server', 'templates']
@@ -19,6 +21,18 @@ textStringsTypes.forEach(textStringsType => {
         languageCodes.forEach(lang2 => compareKeys(textStrings[textStringsType][lang], textStrings[textStringsType][lang2], lang, lang2))
         languageCodes.forEach(lang2 => compareKeysWithinTextStrings(textStrings[textStringsType][lang], textStrings[textStringsType][lang2], lang, lang2))
       })
+
+      it('should not have any birgitta inconsistencies', () => {
+        var errorMessages = {}
+        languageCodes.forEach((lang2, j) => {
+          var errorArray = checkBirgittaInconsistencies(textStrings[textStringsType][lang], textStrings[textStringsType][lang2], lang, lang2)
+          errorArray.forEach(key => {
+            errorMessages[key] = ''
+          })
+        })
+        if (Object.keys(errorMessages).length > 0) console.warn(JSON.stringify(errorMessages, undefined, 2))
+      })
+
       xit('all task templates should not exceed 15 chars', () => {
         checkTemplateLenght(textStrings[textStringsType][lang], lang)
       })
