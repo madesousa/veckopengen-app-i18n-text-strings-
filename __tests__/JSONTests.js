8bb8edabf4cc1b46ce332deb4b4bdbabef4cfac2
andersback <anders.back@me.com>
2017-05-13 10:51:18 +0200

    jest

--- __tests__/JSONTests.js
+++ __tests__/JSONTests.js
@@ -2,15 +2,15 @@ jest.disableAutomock()
 
 describe('TextStrings', () => {
   ['da', 'fi', 'sv', 'nb', 'en', 'nl', 'fr'].forEach(lang => {
-    it(`it should have valid JSON for client/${lang}`, () => {
+    it(`it should have valid JSON for client/${lang}.json`, () => {
       require(`../text_strings/client/${lang}`)
     })
 
-    it(`it should have valid JSON for server/${lang}`, () => {
+    it(`it should have valid JSON for server/${lang}.json`, () => {
       require(`../text_strings/server/${lang}`)
     })
 
-    it(`it should have valid JSON for templates/${lang}`, () => {
+    it(`it should have valid JSON for templates/${lang}.json`, () => {
       require(`../text_strings/templates/${lang}`)
     })
   })
