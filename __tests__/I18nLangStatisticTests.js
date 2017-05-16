769a8aa3f98331746e8443f41eafb187100d26a7
hstamenov <hristo@gimitheapp.com>
2017-05-16 17:02:32 +0300

    skip  it should export in slack the length of text_strings,

--- __tests__/I18nLangStatisticTests.js
+++ __tests__/I18nLangStatisticTests.js
@@ -7,7 +7,7 @@ var Slack = require('node-slack')
 jest.disableAutomock()
 
 describe('TextStrings', () => {
-  it('it should export in slack the length of text_strings', () => {
+  xit('it should export in slack the length of text_strings', () => {
     var stringLengthData
 
     languageCodes.forEach(languageCode => {
