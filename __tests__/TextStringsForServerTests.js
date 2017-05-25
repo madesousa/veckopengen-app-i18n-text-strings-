95c5a464e62fbb3b387269d3288d451a664eb81e
hstamenov <hristo@gimitheapp.com>
2017-05-25 17:16:28 +0300

    added missing text strings and translate updated Krisi to work with gimi-web

--- __tests__/TextStringsForServerTests.js
+++ __tests__/TextStringsForServerTests.js
@@ -1,30 +1,34 @@
 /* eslint no-console:0 */
 import {compareKeys, compareKeysWithinTextStrings, checkTemplateLenght, checkBirgittaInconsistencies} from '../TestUtil'
 import { languageCodes } from '..'
-
+var langCodes = languageCodes
 jest.disableAutomock()
 
-var textStringsTypes = ['server', 'templates']
+var textStringsTypes = ['server', 'templates', 'gimi-web']
 
 var textStrings = {}
 textStringsTypes.forEach(textStringsType => { textStrings[textStringsType] = {} })
-textStringsTypes.forEach(textStringsType => languageCodes.forEach(lang => { textStrings[textStringsType][lang] = require(`../text_strings/${textStringsType}/${lang}`) }))
+textStringsTypes.forEach(textStringsType => {
+  if (textStringsType === 'gimi-web') langCodes = ['da', 'en', 'fi', 'fr', 'nl', 'no', 'se']
+  langCodes.forEach(lang => { textStrings[textStringsType][lang] = require(`../text_strings/${textStringsType}/${lang}`) })
+})
 
 textStringsTypes.forEach(textStringsType => {
   describe(`${textStringsType} TextStrings`, () => {
-    languageCodes.forEach(lang => {
+    if (textStringsType === 'gimi-web') langCodes = ['da', 'en', 'fi', 'fr', 'nl', 'no', 'se']
+    langCodes.forEach(lang => {
       it(`it should return Text Strings for ${textStringsType} ${lang}`, () => {
         expect(textStrings[textStringsType][lang]).not.toEqual(undefined)
       })
 
       it('all textstrings should have a equivalent string in all other languages', () => {
-        languageCodes.forEach(lang2 => compareKeys(textStrings[textStringsType][lang], textStrings[textStringsType][lang2], lang, lang2))
-        languageCodes.forEach(lang2 => compareKeysWithinTextStrings(textStrings[textStringsType][lang], textStrings[textStringsType][lang2], lang, lang2))
+        langCodes.forEach(lang2 => compareKeys(textStrings[textStringsType][lang], textStrings[textStringsType][lang2], lang, lang2))
+        langCodes.forEach(lang2 => compareKeysWithinTextStrings(textStrings[textStringsType][lang], textStrings[textStringsType][lang2], lang, lang2))
       })
 
       it('should not have any birgitta inconsistencies', () => {
         var errorMessages = {}
-        languageCodes.forEach((lang2, j) => {
+        langCodes.forEach((lang2, j) => {
           var errorArray = checkBirgittaInconsistencies(textStrings[textStringsType][lang], textStrings[textStringsType][lang2], lang, lang2)
           errorArray.forEach(key => {
             errorMessages[key] = ''
--- utils/src/krisi.js
+++ utils/src/krisi.js
@@ -1,5 +1,6 @@
 /* eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
 
+// @martin, alright, i would first do this as a seperate project called anna becuase we dont want to screw up sara and this might not work
 var fs = require('fs')
 var translate = require('@google-cloud/translate')({
   projectId: 'gimi-969a7',
@@ -7,63 +8,94 @@ var translate = require('@google-cloud/translate')({
 })
 
 var AnnaHelper = require('./AnnaHelper')
-let templateDir = ['./text_strings/client']
+let templateDir = ['./text_strings/gimi-web']
 
-let RunPolina = (filePath):* => {
-  let getPath = (languageFile) => `${filePath}/${languageCode}`
+let RunAnna = (filePath):* => {
+  var hasPLZTranslate = false
+  let getPath = (file) => `${filePath}/${file}`
   var {toHash, fromHash, translationHelpTemplate} = AnnaHelper
-  let translateLanguageFile = (languageFile) => {
-    if (languageFile.indexOf('.json') === -1) { return void 0 }
+  var translateFrom = 'se.json'
+  let getTranslateFromPath = `${filePath}/` + translateFrom
 
-    var path = getPath(languageFile)
+  let translateTextStringForFile = (file, textId) => {
+    if (file === 'default.json') { return Promise.resolve() }
+    if (file.indexOf('.json') === -1) { return Promise.resolve() }
+
+    if (file === 'se.json') {
+      return Promise.resolve()
+    }
+    /* Translate from given string
+        var path = getPath(file)
+        var TextStrings = fs.readFileSync(path, {encoding: 'utf8'})
+        TextStrings = JSON.parse(TextStrings)
+        var stringToTranslate = TextStrings[textId]
+    */
+
+    // Translate To
+    var path = getPath(file)
     var TextStrings = fs.readFileSync(path, {encoding: 'utf8'})
-    // TextStrings = JSON.parse(TextStrings)
+    TextStrings = JSON.parse(TextStrings)
 
-    var textStringArray = TextStrings.split(':')
-    for (var i = 0; i < TextStrings.length; i++) {
-      console.log(textStringArray[0])
-      var textId = textStringArray[0]
-      var stringToTranslate = textStringArray[0]
+    // Translate From
+    var TranslationString = fs.readFileSync(getTranslateFromPath, {encoding: 'utf8'})
+    TranslationString = JSON.parse(TranslationString)
 
-      if (!stringToTranslate) {
-        console.warn(`Cant find: ${languageCode} in file: ${path}`)
-        return void 0
+    var stringToTranslate = TranslationString[textId]
+
+    // Translation source should not have 'PLZ_TRANSLATE'
+    if (file === translateFrom && stringToTranslate) {
+      if (stringToTranslate.includes('PLZ_TRANSLATE')) {
+        hasPLZTranslate = true
       }
-      var lang = languageFile.replace('TextStrings_', '').replace('.json', '')
-      // hash %1$d to num evals to work with google translate
-      stringToTranslate = toHash(stringToTranslate)
-      i++
-
-      translate.translate(stringToTranslate, lang, (err, translation) => {
-        if (err) {
-          return console.log(`Error on textId ${textId}. message: ${err}. file: ${languageFile}`)
-        }
-        var translatedText = fromHash(translation)
-        console.log(`Translated text: '${stringToTranslate}' to: '${translatedText}' in ${filePath}/${languageFile}`)
-        var re = /PLZ_TRANSLATE/g
-        if (re.test(translatedText)) {
-          TextStrings[textId] = translatedText
-        } else {
-          TextStrings[textId] = `${translationHelpTemplate} ${translatedText}`
-        }
-
-        TextStrings = JSON.stringify(TextStrings, undefined, 2)
-        fs.unlinkSync(path)
-        return fs.writeFileSync(path, TextStrings, {encoding: 'utf8'})
-      })
+    }
+    // Should ignore translation file
+    if (file === translateFrom) {
+      return Promise.resolve()
     }
 
-    return void 0
+    if (!stringToTranslate) {
+      return Promise.reject(new Error(`Cant find textid: ${textId} in file: ${path}`))
+    }
+    var lang = file.replace('TextStrings_', '').replace('.json', '')
+    if (lang === 'nb') {
+      lang = 'no'
+    }
+
+    // hash %1$d to num evals to work with google translate
+    stringToTranslate = toHash(stringToTranslate)
+
+    return translate.translate(stringToTranslate, lang, (err, translation) => {
+      if (err) {
+        return console.log(`Error on textId ${textId}. message: ${err}. file: ${file}`)
+      }
+      if (hasPLZTranslate) {
+        return console.warn(`Should not have PLZ_TRANSLATE in textid: ${textId} in file: ${getTranslateFromPath}`)
+      }
+      var translatedText = fromHash(translation)
+      console.log(`Translated text: '${stringToTranslate}' to: '${translatedText}' in ${filePath}/${file}`)
+      var re = /PLZ_TRANSLATE/g
+      if (re.test(translatedText)) {
+        TextStrings[textId] = translatedText
+      } else {
+        TextStrings[textId] = `${translationHelpTemplate} ${translatedText}`
+      }
+
+      TextStrings = JSON.stringify(TextStrings, undefined, 2)
+      fs.unlinkSync(path)
+      return fs.writeFileSync(path, TextStrings, {encoding: 'utf8'})
+    })
   }
 
-  let languageCode = process.argv[2]
+  let textIdToTranslate = process.argv[2]
 
-  if (!languageCode) { console.log('use: npm run poli -- <languageCode>') }
+  if (!textIdToTranslate) { console.log('use: npm run anna -- <text_id>') }
 
-  if (languageCode) {
-    translateLanguageFile(languageCode)
+  if (textIdToTranslate) {
+    Promise.all(fs.readdirSync(filePath).map((file) => translateTextStringForFile(file, textIdToTranslate)))
+      .then(() => console.log('saved Successfully :)'))
+      .catch((err) => console.error(err.message))
   }
 }
 templateDir.forEach((filePath) => {
-  RunPolina(filePath)
+  RunAnna(filePath)
 })
